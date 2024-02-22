1615a8bebae95b73ce9727e3dbb08e3530a06362
Megha Komarraju <47645860+Megha-Komarraju@users.noreply.github.com>
2024-02-22 14:30:16 -0500

    Update chatbot.py

--- Python Projects/Meg's Chat Bot/chatbot.py
+++ Python Projects/Meg's Chat Bot/chatbot.py
@@ -1,4 +1,15 @@
-import requirements.txt
+import os
+import nltk
+import ssl
+import streamlit as st
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+import joblib
+
+# Import emoji library
+from emoji import emojize
 
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath("nltk_data"))
